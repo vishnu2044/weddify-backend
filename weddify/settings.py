971c43634bc30460ba882e81902c7d2ced4baf86
vishnu narayanan <vishnunarayanan2044@gmail.com>
2023-12-08 11:02:28 +0530

    bugg

--- weddify/settings.py
+++ weddify/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 DEBUG = config('DEBUG', cast=bool, default = True)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = []
+
 
 
 # Application definition
